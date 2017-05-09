6d3355b4d72798e367541459d31b5bed4fa68258
Jules Terrien <julesterrien@gmail.com>
2017-05-09 09:40:17 -0700

    Add babelrc.

--- build/index.js
+++ build/index.js
@@ -1,17 +1,30 @@
-const CHAIN = 'CHAIN';
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+	value: true
+});
+var CHAIN = 'CHAIN';
 
 /**
 * chainMiddleware
 * Add this middleware to your redux store to use the chain action
 */
-export const chainMiddleware = ({ dispatch, getState }) => next => action => {
-	if (action.type === CHAIN) {
-		if (!Array.isArray(action.actions)) {
+var chainMiddleware = exports.chainMiddleware = function chainMiddleware(_ref) {
+	var dispatch = _ref.dispatch,
+	    getState = _ref.getState;
+	return function (next) {
+		return function (action) {
+			if (action.type === CHAIN) {
+				if (!Array.isArray(action.actions)) {
+					return next(action);
+				}
+				return action.actions.forEach(function (act) {
+					return next(act);
+				});
+			}
 			return next(action);
-		}
-		return action.actions.forEach(act => next(act));
-	}
-	return next(action);
+		};
+	};
 };
 
 /**
@@ -19,9 +32,15 @@ export const chainMiddleware = ({ dispatch, getState }) => next => action => {
 * @param	{Objects}	args	the action creators to be chained
 * @return {Object} 	an action creator object
 */
-export const chain = (...args) => ({
-	type: CHAIN,
-	actions: args
-});
+var chain = exports.chain = function chain() {
+	for (var _len = arguments.length, args = Array(_len), _key = 0; _key < _len; _key++) {
+		args[_key] = arguments[_key];
+	}
 
-export default { chainMiddleware, chain };
+	return {
+		type: CHAIN,
+		actions: args
+	};
+};
+
+exports.default = { chainMiddleware: chainMiddleware, chain: chain };
